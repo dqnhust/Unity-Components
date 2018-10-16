19a6d739041bf3c835b1b0d2bc85dc381f3fe889
Duong Quang Nam <quangnam.a10@gmail.com>
2018-10-16 12:44:22 +0700

    Update popupManager

--- Popup/Popup.cs
+++ Popup/Popup.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Animator), typeof(CanvasGroup))]
 public abstract class Popup:MonoBehaviour
 {
     [SerializeField] private Animator anim;
