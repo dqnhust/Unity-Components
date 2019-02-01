44771d7044fbf1f2d222e99b87f76b771be1b7af
Duong Quang Nam <quangnam.a10@gmail.com>
2019-02-01 14:43:44 +0700

    Update

---
+++ EventNumber/EventNumber.cs
@@ -0,0 +1,30 @@
+﻿#pragma warning disable 0649
+using System;
+
+[Serializable]
+public class EventNumber<T>
+{
+    private T value;
+
+    public T Value
+    {
+        get
+        {
+            return value;
+        }
+        set
+        {
+            this.value = value;
+            OnValueChanged?.Invoke();
+        }
+    }
+
+
+    [field: NonSerializedAttribute]
+    public event System.Action OnValueChanged;
+
+    public EventNumber(T value)
+    {
+        this.value = value;
+    }
+}
