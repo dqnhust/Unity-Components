218674027165c082c772144658f2fbcc4e3a7572
duongquangnam <quangnam.a10@gmail.com>
2019-07-14 22:33:52 +0700

    Fps View

---
+++ FpsView/FPSView.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSView : MonoBehaviour
+{
+    [SerializeField] private Text text;
+
+    private float CurrentFPS => 1f / Time.deltaTime;
+    private float AverageFPS
+    {
+        get
+        {
+            _averageFPS = Mathf.Lerp(_averageFPS, CurrentFPS, 0.1f);
+            return Mathf.RoundToInt(_averageFPS);
+        }
+    }
+    private float _averageFPS = 60;
+
+    private void Update()
+    {
+        text.text = AverageFPS.ToString();
+    }
+}
