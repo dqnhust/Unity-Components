40ce4323bdda18b48c00e5e2bd582103e0f2ea7a
namdq_ptt <namdq@phugroup.com>
2020-04-08 14:24:28 +0700

    Fog

---
+++ FogFake/FogManager.cs
@@ -0,0 +1,29 @@
+﻿#pragma warning disable 0649
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogManager : MonoBehaviour
+{
+    [SerializeField] private float minFogDistance;
+    [SerializeField] private float maxFogDistance;
+    [SerializeField] private Color fogColor;
+
+    [ContextMenu("Set Params")]
+    private void SetParams()
+    {
+        Shader.SetGlobalFloat("_MinFogDistance", minFogDistance);
+        Shader.SetGlobalFloat("_MaxFogDistance", maxFogDistance);
+        Shader.SetGlobalColor("_FogColor", fogColor);
+    }
+
+    private void Awake()
+    {
+        SetParams();
+    }
+
+    private void OnValidate()
+    {
+        SetParams();
+    }
+}
