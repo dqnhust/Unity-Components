56467e3dfa150d08579e4fac6321d157c74e311b
Duong Quang Nam <quangnam.a10@gmail.com>
2020-06-29 22:30:09 +0700

    Update

---
+++ AbstractEventNumberView/AbstractEventNumberView.cs
@@ -0,0 +1,33 @@
+﻿#pragma warning disable 0649
+using UnityEngine;
+
+public abstract class AbstractEventNumberView<T> : MonoBehaviour
+{
+    [SerializeField] protected TextFormatView textView;
+
+    protected abstract EventNumber<T> Data
+    {
+        get;
+    }
+
+    protected abstract string StringValue
+    {
+        get;
+    }
+
+    protected virtual void OnEnable()
+    {
+        Data.OnValueChanged += UpdateUI;
+        UpdateUI();
+    }
+
+    protected virtual void OnDisable()
+    {
+        Data.OnValueChanged -= UpdateUI;
+    }
+
+    private void UpdateUI()
+    {
+        textView.SetString(StringValue);
+    }
+}
