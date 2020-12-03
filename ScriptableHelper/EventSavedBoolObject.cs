8db8b7cabcfc2cd2f2d44a01cab5bedc9b5cda31
Duong Quang Nam <quangnam.a10@gmail.com>
2020-12-03 19:19:06 +0700

    Update

---
+++ ScriptableHelper/EventSavedBoolObject.cs
@@ -0,0 +1,23 @@
+﻿#pragma warning disable 0649
+using UnityEngine;
+
+namespace GameEvent
+{
+    [CreateAssetMenu(menuName = "GameData/EventSavedBoolObject", fileName = "EventSavedBoolObject", order = 0)]
+    public class EventSavedBoolObject : AbstractEventObject<bool>
+    {
+        [SerializeField] private bool defaultValue;
+
+        private bool SavedValue
+        {
+            get => PlayerPrefs.GetInt(name + "Scriptable", defaultValue ? 1 : 0) == 1;
+            set => PlayerPrefs.SetInt(name + "Scriptable", value ? 1 : 0);
+        }
+
+        protected override bool Value
+        {
+            get => SavedValue;
+            set => SavedValue = value;
+        }
+    }
+}
--- SoundManager/AbstractSoundManager.cs
+++ SoundManager/AbstractSoundManager.cs
@@ -1,121 +1,108 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public abstract class AbstractSoundManager : MonoBehaviour
+namespace SoundManager
 {
-    public float MusicVolume
+    public abstract class AbstractSoundManager : MonoBehaviour
     {
-        get
+        private AudioSource _effectSource;
+        private Transform _loopStore;
+
+        private bool _initialized;
+
+        protected float SoundVolume
         {
-            return PlayerPrefs.GetFloat("MusicVolume", 1);
-        }
-        set
-        {
-            if (value.Equals(MusicVolume)) return;
-            PlayerPrefs.SetFloat("MusicVolume", value);
-            InvokeEvent(OnMusicVolumeChange);
-        }
-    }
-
-    public float SoundVolume
-    {
-        get
-        {
-            return PlayerPrefs.GetFloat("SoundVolume", 1);
-        }
-        set
-        {
-            if (value.Equals(SoundVolume)) return;
-            PlayerPrefs.SetFloat("SoundVolume", value);
-            InvokeEvent(OnSoundVolumeChange);
-        }
-    }
-
-    public event System.Action OnMusicVolumeChange;
-    public event System.Action OnSoundVolumeChange;
-
-    protected AudioSource effectSource;
-    protected Transform loopStore;
-
-    protected void InvokeEvent(System.Action callBack)
-    {
-        if (callBack != null)
-        {
-            callBack.Invoke();
-        }
-    }
-
-    protected bool inited = false;
-
-    protected virtual void Awake()
-    {
-        Init();
-    }
-
-    protected void Init()
-    {
-        if (inited) return;
-        if (effectSource == null)
-        {
-            effectSource = gameObject.AddComponent<AudioSource>();
+            get => _effectSource.volume;
+            set => _effectSource.volume = value;
         }
 
-        if (loopStore == null)
-        {
-            loopStore = transform;
-        }
-        inited = true;
-    }
+        private float _musicVolume;
 
-    protected void PlayEffect(AudioClip clip)
-    {
-        if (!inited)
+        protected float MusicVolume
+        {
+            get => _musicVolume;
+            set
+            {
+                _musicVolume = value;
+                foreach (var data in LoopDict)
+                {
+                    data.Value.volume = value;
+                }
+            }
+        }
+
+        protected virtual void Awake()
         {
             Init();
         }
-        effectSource.PlayOneShot(clip);
-    }
 
-    private Dictionary<AudioClip, AudioSource> loopDict = new Dictionary<AudioClip, AudioSource>();
-
-    protected AudioSource PlayLoop(AudioClip clip)
-    {
-        AudioSource source;
-        bool exist = loopDict.TryGetValue(clip, out source);
-        if (exist)
+        protected void Init()
         {
-            source.Play();
+            if (_initialized) return;
+            if (_effectSource == null)
+            {
+                _effectSource = gameObject.AddComponent<AudioSource>();
+            }
+
+            if (_loopStore == null)
+            {
+                _loopStore = transform;
+            }
+
+            _initialized = true;
         }
-        else
+
+        protected void PlayEffect(AudioClip clip)
         {
-            source = loopStore.gameObject.AddComponent<AudioSource>();
-            source.loop = true;
-            source.playOnAwake = false;
-            source.clip = clip;
-            source.Play();
-            loopDict.Add(clip, source);
+            if (!_initialized)
+            {
+                Init();
+            }
+
+            _effectSource.PlayOneShot(clip);
         }
-        return source;
-    }
 
-    protected void StopLoop(AudioSource source)
-    {
-        source.Stop();
-    }
+        private Dictionary<AudioClip, AudioSource> LoopDict { get; } = new Dictionary<AudioClip, AudioSource>();
 
-    protected void StopLoop(AudioClip clip)
-    {
-        AudioSource source = GetLoopingSource(clip);
-        if (source != null)
+        protected AudioSource PlayLoop(AudioClip clip)
+        {
+            bool exist = LoopDict.TryGetValue(clip, out var source);
+            if (exist)
+            {
+                source.Play();
+            }
+            else
+            {
+                source = _loopStore.gameObject.AddComponent<AudioSource>();
+                source.loop = true;
+                source.volume = MusicVolume;
+                source.playOnAwake = false;
+                source.clip = clip;
+                source.Play();
+                LoopDict.Add(clip, source);
+            }
+
+            return source;
+        }
+
+        protected void StopLoop(AudioSource source)
         {
             source.Stop();
         }
-    }
 
-    protected AudioSource GetLoopingSource(AudioClip clip)
-    {
-        AudioSource source;
-        loopDict.TryGetValue(clip, out source);
-        return source;
+        protected void StopLoop(AudioClip clip)
+        {
+            AudioSource source = GetLoopingSource(clip);
+            if (source != null)
+            {
+                source.Stop();
+            }
+        }
+
+        protected AudioSource GetLoopingSource(AudioClip clip)
+        {
+            LoopDict.TryGetValue(clip, out var source);
+            return source;
+        }
     }
 }
