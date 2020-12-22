9075bb55a1892393965a58a22944eb3a5866497a
Duong Quang Nam <quangnam.a10@gmail.com>
2020-12-22 08:09:33 +0700

    Update

---
+++ AdmobAdsManager/AdsController.cs
@@ -0,0 +1,125 @@
+﻿#pragma warning disable 0649
+using System;
+using System.Collections;
+using GameEvent;
+using GameMode;
+using InAppReview;
+using UnityEngine;
+
+namespace Ads
+{
+    public class AdsController : MonoBehaviour
+    {
+        [SerializeField] private AdsManager adsManager;
+        [SerializeField] private EventSavedBoolObject boughIAP;
+        [SerializeField] private GameManagerEvent gameManagerEvent;
+        [SerializeField] private InAppReviewController inAppReviewController;
+
+        private int CallShowInterstitialCount
+        {
+            get => PlayerPrefs.GetInt("CallShowInterstitialCount", 0);
+            set => PlayerPrefs.SetInt("CallShowInterstitialCount", value);
+        }
+
+        private void Awake()
+        {
+            // if (!boughIAP.GetValue())
+            // {
+            // }
+            adsManager.Init(true, true, true);
+        }
+
+        private void OnEnable()
+        {
+            boughIAP.OnValueChanged += OnBoughIAPValueChanged;
+            gameManagerEvent.EventShowGameOverUI += OnGameOver;
+            // gameManagerEvent.EventGamePaused += OnGamePause;
+            if (!boughIAP.GetValue())
+            {
+                adsManager.ShowBanner();
+            }
+            else
+            {
+                adsManager.HideBanner();
+            }
+
+            gameManagerEvent.EventShowReward += OnHaveCallShowReward;
+        }
+
+        private void OnDisable()
+        {
+            gameManagerEvent.EventShowGameOverUI -= OnGameOver;
+            // gameManagerEvent.EventGamePaused -= OnGamePause;
+            boughIAP.OnValueChanged -= OnBoughIAPValueChanged;
+            gameManagerEvent.EventShowReward -= OnHaveCallShowReward;
+        }
+
+        private void OnHaveCallShowReward()
+        {
+            adsManager.ShowRewarded(gameManagerEvent.OnEventGotRewarded, gameManagerEvent.OnEventCloseReward);
+        }
+
+        private void OnBoughIAPValueChanged()
+        {
+            if (boughIAP.GetValue())
+            {
+                adsManager.HideBanner();
+            }
+        }
+
+        // private void OnGamePause()
+        // {
+        //     if (boughIAP.GetValue()) return;
+        //     if (CallShowInterstitialCount > 1)
+        //     {
+        //         adsManager.ShowInterstitial();
+        //     }
+        // }
+
+        private void OnGameOver(IGameMode obj)
+        {
+            CallShowInterstitialCount++;
+            if (boughIAP.GetValue())
+            {
+                return;
+            }
+
+            StartCoroutine(IeShowDelay(1f));
+            // adsManager.ShowInterstitial();
+        }
+
+
+        private IEnumerator IeShowDelay(float delay)
+        {
+            if (delay > 0f)
+            {
+                yield return new WaitForSeconds(delay);
+            }
+
+            if (inAppReviewController.Showing)
+            {
+                yield break;
+            }
+
+            adsManager.ShowInterstitial();
+        }
+
+        private float lastTimePause;
+
+        private void OnApplicationPause(bool pauseStatus)
+        {
+            if (pauseStatus)
+            {
+                lastTimePause = Time.realtimeSinceStartup;
+            }
+            else
+            {
+                var deltaTime = Time.realtimeSinceStartup - lastTimePause;
+                if (deltaTime > 20)
+                {
+                    adsManager.ShowInterstitial();
+                }
+            }
+        }
+    }
+}
---
+++ AdmobAdsManager/AdsManager.cs
@@ -0,0 +1,172 @@
+﻿#pragma warning disable 0649
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using GoogleMobileAds.Api;
+using UnityEngine;
+
+namespace Ads
+{
+    public class AdsManager : MonoBehaviour
+    {
+        [SerializeField] private string bannerId;
+        [SerializeField] private string interstitialId;
+        [SerializeField] private string rewardedId;
+
+        public void Init(bool enableBanner, bool enableInterstitial, bool enableRewarded)
+        {
+            MobileAds.Initialize(initStatus => { Debug.Log("Admob Mobile Ads initialized, Status: " + initStatus); });
+            if (enableBanner)
+            {
+                RequestBanner();
+            }
+
+            if (enableInterstitial)
+            {
+                RequestAndLoadInterstitialAd();
+            }
+
+            if (enableRewarded)
+            {
+                RequestAndLoadRewardedAd();
+            }
+        }
+
+        // ReSharper disable once UnusedMember.Global
+        public void ShowBanner()
+        {
+            _bannerView?.Show();
+        }
+
+        // ReSharper disable once MemberCanBePrivate.Global
+        public void HideBanner()
+        {
+            _bannerView?.Hide();
+        }
+
+        // ReSharper disable once UnusedMember.Global
+        public void ShowInterstitial()
+        {
+            if (_interstitialAd != null && _interstitialAd.IsLoaded())
+            {
+                _interstitialAd.Show();
+            }
+        }
+
+        // ReSharper disable once UnusedMember.Global
+        public void ShowRewarded(Action callBackDone, Action callBackClose)
+        {
+            _gotRewarded = false;
+            _actionDoneReward = callBackDone;
+            _actionCloseReward = callBackClose;
+            if (_rewardedAd != null && _rewardedAd.IsLoaded())
+            {
+                _rewardedAd.Show();
+            }
+        }
+
+        #region Banner Setup
+
+        private BannerView _bannerView;
+
+        private AdRequest GetRequest()
+        {
+            return new AdRequest.Builder().AddTestDevice("597AE8AFFA7FF76120D8F02976C52C23").Build();
+        }
+
+        private void RequestBanner()
+        {
+            _bannerView?.Destroy();
+            _bannerView = new BannerView(bannerId, AdSize.Banner, AdPosition.Bottom);
+            _bannerView.LoadAd(GetRequest());
+            HideBanner();
+        }
+
+        #endregion
+
+        #region Interstitial Setup
+
+        private InterstitialAd _interstitialAd;
+
+        // ReSharper disable once MemberCanBePrivate.Global
+        public void RequestAndLoadInterstitialAd()
+        {
+            _interstitialAd?.Destroy();
+            _interstitialAd = new InterstitialAd(interstitialId);
+            _interstitialAd.OnAdClosed += (sender, args) => RequestAndLoadInterstitialAd();
+            _interstitialAd.OnAdFailedToLoad += (sender, args) =>
+            {
+                InvokeOnMainThread(() =>
+                    InvokeDelay(15f, RequestAndLoadInterstitialAd)
+                );
+            };
+            _interstitialAd.LoadAd(GetRequest());
+        }
+
+        #endregion
+
+        #region Rewarded Setup
+
+        private RewardedAd _rewardedAd;
+        private bool _gotRewarded;
+
+        private void RequestAndLoadRewardedAd()
+        {
+            // _gotRewarded = false;
+            _rewardedAd = new RewardedAd(rewardedId);
+
+            _rewardedAd.OnAdClosed += (sender, args) =>
+            {
+                InvokeOnMainThread(() => InvokeDelay(0.1f, () =>
+                {
+                    Debug.LogError("Ad Closed!");
+                    InvokeOnMainThread(_actionCloseReward);
+                    RequestAndLoadRewardedAd();
+                }));
+            };
+            _rewardedAd.OnUserEarnedReward += (sender, args) =>
+            {
+                InvokeOnMainThread(_actionDoneReward);
+                // _gotRewarded = true;
+                Debug.LogError("Got Reward!");
+            };
+            _rewardedAd.LoadAd(GetRequest());
+        }
+
+        private Action _actionDoneReward;
+        private Action _actionCloseReward;
+
+        #endregion
+
+        private readonly List<Action> _listMethodWillBeInvokeOnMainThread = new List<Action>();
+
+        private void InvokeOnMainThread(Action method)
+        {
+            _listMethodWillBeInvokeOnMainThread.Add(method);
+        }
+
+        private void Update()
+        {
+            if (_listMethodWillBeInvokeOnMainThread.Count > 0)
+            {
+                foreach (var method in _listMethodWillBeInvokeOnMainThread)
+                {
+                    method?.Invoke();
+                }
+
+                _listMethodWillBeInvokeOnMainThread.Clear();
+            }
+        }
+
+        private void InvokeDelay(float delay, Action method)
+        {
+            StartCoroutine(IeInvokeDelay(delay, method));
+        }
+
+        private IEnumerator IeInvokeDelay(float delay, Action method)
+        {
+            yield return new WaitForSeconds(delay);
+            method?.Invoke();
+        }
+    }
+}
