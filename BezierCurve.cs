07cb33c3c022647162fecb0b048ad8238ce86b7e
Duong Quang Nam <quangnam.a10@gmail.com>
2021-01-10 20:38:00 +0700

    Update

---
+++ BezierCurve.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierCurve
+{
+    private readonly Vector3[] _points;
+
+    public BezierCurve(params Vector3[] points)
+    {
+        this._points = points;
+    }
+
+    public Vector3 GetPoint(float percent)
+    {
+        var result = new List<Vector3>(_points);
+        while (result.Count > 1)
+        {
+            var n = result.Count;
+            for (int i = 0, j = 1; j < n; i++, j++)
+            {
+                result.Add(Vector3.Lerp(result[i], result[j], percent));
+            }
+
+            result.RemoveRange(0, n);
+        }
+
+        return result[0];
+    }
+}
---
+++ Vector3Range.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Vector3Range
+{
+    public Vector3 min;
+    public Vector3 max;
+
+    public Vector3 GetValue() =>
+        new Vector3(Random.Range(min.x, max.x), Random.Range(min.y, max.y), Random.Range(min.z, max.z));
+}
